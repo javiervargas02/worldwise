a9c2608518c0f57d15ff255096bd4ad3c9622944
javiervargas02 <javier.v.021204@gmail.com>
2024-09-28 15:12:57 -0600

    Add all needed files to start project

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Homepage from "./routes/Homepage";
+import Product from "./routes/Product";
+import Pricing from "./routes/Pricing";
+import AppLayout from "./routes/AppLayout";
+import Login from "./routes/Login";
+
 export default function App() {
   return (
-    <div>
-      <h1>Hello, World!</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="*" element={<Homepage />} />
+        <Route path="/product" element={<Product />} />
+        <Route path="/pricing" element={<Pricing />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/app" element={<AppLayout />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
---
+++ src/components/AppNav.jsx
@@ -0,0 +1,7 @@
+export default function AppNav() {
+  return (
+    <div>
+      <h1>App Navigation</h1>
+    </div>
+  );
+}
